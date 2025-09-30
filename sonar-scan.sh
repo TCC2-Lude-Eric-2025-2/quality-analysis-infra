7c278913395e74af8972f7891bf5faccdf9d288a
Eric-chagas <eric_chagas12@outlook.com>
2025-09-29 22:32:30 -0300

    updating sonar properties and test scan script

--- sonar-scan.sh
+++ sonar-scan.sh
@@ -5,6 +5,12 @@ set -e
 echo "Building scanner image..."
 docker build -t zulip-sonar-scanner -f Dockerfile-sonar-scanner .
 
+# Get vagrant virtualized environment up and running
+vagrant up --provider=docker
+
+# Run tests inside vagrant environment
+vagrant ssh -c "./tools/test-backend --coverage --xml-report"
+
 # --- Run SonarQube scanner ---
 echo "Running scanner..."
 docker run --rm \
